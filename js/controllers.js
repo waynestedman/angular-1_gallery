15973e3e112d5a0dfac0cdc55fb22f6cdd14973e
Wayne Stedman <wayne@pacificwebfoundry.com>
2016-10-11 18:09:58 -0700

    sorting
    Added sorting options; some css fine-tuning.

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('GalleryController', ['$scope', '$http', function($scope, $http) {
     $http.get('js/data.json').success(function(data) {
         $scope.gallery = data;
+        $scope.galleryOrder = 'title';
     });
 }]);
